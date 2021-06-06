70db38d5574c977293ca1d340adbe5888daff879
llxSKyWALKeRxll <19bcs2190@gmail.com>
2021-06-07 04:37:27 +0530

    Module-4 assignment completed (Hard difficulty xD).

--- trying_js/script.js
+++ trying_js/script.js
@@ -1,26 +1,16 @@
-var x = 'Hello there, this is my first print statement on JavaScript!';
-console.log(x);
-function a1 () {
-	console.log("Inside function a1.\nChecking new line operator!");
-	console.log(x);
-	function a2() {
-		console.log("Inside a2.");
-		console.log(x);
-	}
-	x = "Lul";
-	a2();
+function a1(x) {
+	return x * 2;
 }
-a1();
-console.log(undefined / 5);
-console.log(null / 10);
-console.log('Hellow' + ' there!');
-console.log('Value of x: '+x);
+a1.version = 'v-1.03';
 
-// for (i = 0; i < 5; i++) {
-// 	console.log(i);
-// }
+function a2(x) {
+	return x / 2;
+}
 
-// for (var i = 5; i < 10; i++) {
-// 	console.log(i);
-// }
-// console.log(i);
+function perform(x, operation) {
+	return operation(x);
+}
+
+console.log(perform(10, a1));
+console.log(perform(10, a2));
+console.log(Math.PI);
